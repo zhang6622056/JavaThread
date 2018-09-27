689499d288c5f6d8f314e48b9294bbb80d4c44f4
zhanglei-js <zhanglei-js@secoo.com>
2018-09-27 22:59:28 +0800

    final内存语义案例理解

---
+++ src/main/java/base/ch02/FinalExample.java
@@ -0,0 +1,44 @@
+package base.ch02;
+
+/**
+ * -禁止把final域的写重排序到构造函数之外   finalField = 1;不能在构造函数之外。
+ * (就是说，你在对象外面读取到这个对象时，final域已经完成赋值)
+ *
+ * -在一个线程中，初次读对象引用于初次读该对象包含final属性引用。不可重排序
+ * -final域读案例存在依赖关系，但某些CPU还是不会遵守，所以
+ *
+ * Created by Nero on 2018-09-27.
+ */
+public class FinalExample {
+
+    int norml;
+    final int finalField;    //final 变量
+    static FinalExample obj;
+
+    public FinalExample(){
+        norml = 2;      //写普通域
+        finalField = 1;          //final域变量写入
+        //storestore屏障
+        //return
+    }
+
+
+    public static void writer(){        //写线程A执行
+        obj = new FinalExample();
+    }
+
+
+    public static void reader(){        //读线程B执行
+        FinalExample object = obj;  //读对象引用
+        int a = object.norml;       //读普通域
+        //readread
+        int b = object.finalField;  //读final域
+    }
+
+
+
+
+
+
+
+}
---
+++ src/main/java/base/ch02/FinalReferenceExample.java
@@ -0,0 +1,53 @@
+package base.ch02;
+
+/**
+ * final域为引用类型的内存语义
+ * 构造函数内对final域的写入，与构造函数外，把构造出来的对象赋值给某个引用变量，这两个操作不可以重排序，即1和3不可以重排序
+ *
+ *
+ *
+ * Created by 止水 on 2018-09-27.
+ */
+public class FinalReferenceExample {
+
+    final int[] finalRefer;
+    static FinalReferenceExample obj;
+
+    public FinalReferenceExample(){
+        finalRefer = new int[1];        //1...引用成员final域写入
+        finalRefer[0] = 1;              // 2....final域引用的成员域的写入
+        //storestore
+        //return
+    }
+
+
+    public static void writerOne(){     //写线程A
+        obj = new FinalReferenceExample();  // 3 构造函数外，引用赋值给某个对象
+    }
+
+    public static void writerTwo(){     //写线程B
+        obj.finalRefer[0] = 2;
+    }
+
+    public static void reader(){        //读线程C
+        if (null != obj){
+            int temp = obj.finalRefer[0];
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
