a5f50f3e515cd70b99c56561c8b31222b5bb5b33
zhanglei-js <zhanglei-js@secoo.com>
2018-09-29 22:31:06 +0800

    java thread....

--- src/main/java/base/api/synchronizeds/simplebytecode/SynchronizedBytecode.java
+++ src/main/java/base/api/synchronizeds/simplebytecode/SynchronizedBytecode.java
@@ -1,23 +1,59 @@
 package base.api.synchronizeds.simplebytecode;
 
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+
 /**
  * 从最简单入手JVM指令集
- *
- *
  * Created by nero on 2018-09-29.
  */
-public class SynchronizedBytecode {
-    private int i = 0;
-    private synchronized void count(){
-        i = i + 1;
+public class SynchronizedBytecode implements Runnable{
+    private Map map;
+    private static CountDownLatch countDownLatch = new CountDownLatch(2);
+
+
+    private Map getMap(){
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        synchronized(this){
+            System.out.println(Thread.currentThread().getName()+","+map);
+            if(map == null){
+                map = new HashMap<>();
+            }
+        }
+
+        return map;
+    }
+
+    @Override
+    public void run() {
+        getMap();
     }
 
 
+    public static void main(String[] args) {
+        SynchronizedBytecode synchronizedBytecode = new SynchronizedBytecode();
+
+        Hashtable hashtable = new Hashtable();
 
 
-
-
-
+        Thread thread1 = new Thread(synchronizedBytecode);
+        Thread thread2 = new Thread(synchronizedBytecode);
+        thread1.start();
+        thread2.start();
+        countDownLatch.countDown();
+        try {
+            Thread.sleep(1000l);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        countDownLatch.countDown();
+    }
 
 
 }
