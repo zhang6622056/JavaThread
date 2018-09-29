0f09744f6804becb591b752f509dc5c404e327e0
zhanglei-js <zhanglei-js@secoo.com>
2018-09-29 15:55:10 +0800

    synchronized 范围

---
+++ src/main/java/base/api/synchronizeds/simplebytecode/SynchronizedBytecode.java
@@ -0,0 +1,23 @@
+package base.api.synchronizeds.simplebytecode;
+
+/**
+ * 从最简单入手JVM指令集
+ *
+ *
+ * Created by nero on 2018-09-29.
+ */
+public class SynchronizedBytecode {
+    private int i = 0;
+    private synchronized void count(){
+        i = i + 1;
+    }
+
+
+
+
+
+
+
+
+
+}
